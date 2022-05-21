3e950db5683bb066f28c216d3d574464844cdfe2
Ryan Hiltabrand <ryhiltabrand99@gmail.com>
2022-05-21 17:43:15 -0400

    start

---
+++ sleeperfantasybot.js
@@ -0,0 +1,31 @@
+// Setup our environment variables via dotenv
+require('dotenv').config()
+// Import relevant classes from discord.js
+const { Client, Intents } = require('discord.js');
+// Instantiate a new client with some necessary parameters.
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const fs = require('node:fs');
+const path = require('node:path');
+// Notify progress
+client.on('ready', function(e){
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
+// Authenticate
+
+client.login(process.env.DISCORD_TOKEN)
+
